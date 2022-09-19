ebd1d82b117caf75447550b8fbbca1b1aebeabf0
YoginiMac12 <113415806+YoginiMac12@users.noreply.github.com>
2022-09-19 23:17:10 +0530

    Add files via upload

---
+++ VowelConsonantProgram/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VowelConsonantProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch;
+            Console.WriteLine("Enter a Character");
+            ch = Convert.ToChar(Console.ReadLine());
+
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') 
+            {
+                Console.WriteLine("Character is Vowel");
+            }
+            else
+            {
+                Console.WriteLine("Character is Consonant");
+            }
+            Console.ReadLine();
+        }
+    }
+}
